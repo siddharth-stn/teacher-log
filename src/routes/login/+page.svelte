3334db9995d38877fd89e06c3a41e4ef19420365
Siddharth Pande <sidarjunstn.sp@gmail.com>
2023-04-28 16:08:46 +0530

    started login page

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,25 @@
+<div>
+  <form>
+    <div>
+      <div class="flex flex-col">
+        <input type="radio" id="admin" /><label for="admin">Admin</label>
+        <input type="radio" id="employee" /><label for="employee"
+          >Employee</label
+        >
+      </div>
+
+      <div class="flex flex-col">
+        <label for="email">
+          <input type="email" placeholder="Enter your email..." /></label
+        >
+        <label for="password">
+          <input type="password" placeholder="Enter password..." /></label
+        >
+      </div>
+
+      <div>
+        <button type="submit">login</button>
+      </div>
+    </div>
+  </form>
+</div>
