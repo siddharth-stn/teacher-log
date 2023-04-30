40dbc8de58355ffd3d66bd130d9d9633f4ff5148
Siddharth Pande <sidarjunstn.sp@gmail.com>
2023-04-30 23:27:39 +0530

    login page for mobile created

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,24 +1,43 @@
-<div class="max-h-screen flex items-center">
-  <form class="w-11/12 mx-auto">
-    <div class="border-solid border-2 border-red-500 flex flex-col gap-4">
-      <div class="flex flex-col mx-auto">
-        <label for="admin"><input type="radio" id="admin" />Admin</label>
-        <label for="employee"
-          ><input type="radio" id="employee" />Employee</label
-        >
-      </div>
-
-      <div class="flex flex-col">
-        <label for="email">
-          <input type="email" placeholder="Enter your email..." /></label
-        >
-        <label for="password">
-          <input type="password" placeholder="Enter password..." /></label
-        >
-      </div>
-
+<div class="h-screen grid place-items-center text-lg">
+  <form class="w-11/12 mx-auto bg-white">
+    <div class="flex flex-col gap-4 shadow-xl rounded-md">
       <div>
-        <button type="submit">login</button>
+        <div
+          class="bg-violet-400 grid place-items-center rounded-t-md text-white px-4 py-8"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            class="w-12 h-12"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M15.75 1.5a6.75 6.75 0 00-6.651 7.906c.067.39-.032.717-.221.906l-6.5 6.499a3 3 0 00-.878 2.121v2.818c0 .414.336.75.75.75H6a.75.75 0 00.75-.75v-1.5h1.5A.75.75 0 009 19.5V18h1.5a.75.75 0 00.53-.22l2.658-2.658c.19-.189.517-.288.906-.22A6.75 6.75 0 1015.75 1.5zm0 3a.75.75 0 000 1.5A2.25 2.25 0 0118 8.25a.75.75 0 001.5 0 3.75 3.75 0 00-3.75-3.75z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </div>
+        <div class="flex flex-col gap-4 py-4 px-8">
+          <input
+            type="email"
+            placeholder="Enter your email..."
+            class="border-b border-gray-400 px-4 py-1 focus:outline-none focus:border-b-orange-400"
+          />
+          <input
+            type="password"
+            placeholder="Enter your password..."
+            class="border-b border-gray-400 px-4 py-1 focus:outline-none focus:border-b-orange-400"
+          />
+        </div>
+
+        <div class="p-4 flex justify-center">
+          <button
+            type="submit"
+            class="border border-violet-300 rounded-md px-3 py-1 mb-4 text-white bg-violet-400 hover:border-black hover:bg-white hover:text-violet-500"
+            >Login</button
+          >
+        </div>
       </div>
     </div>
   </form>
